3b0816c7c0ed82c93bc2e0e0fc8fad7b31ea94e9
PhuLoc25102003 <155551286+PhuLoc25102003@users.noreply.github.com>
2024-04-23 15:39:10 +0700

    ver_1.0

---
+++ Tenant.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tenant {
+    public User user = new User();
+    private List<RentalContractAndRealse> ListOfContract = new ArrayList<>();
+
+}
